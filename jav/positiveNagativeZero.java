808d468c5b7e69aa721090e37e624f676ea1b3e0
Aadil-Abdirizak <65042842+Aadil-Abdirizak@users.noreply.github.com>
2020-07-12 14:41:33 +0300

    Add files via upload

---
+++ jav/positiveNagativeZero.java
@@ -0,0 +1,14 @@
+public class positiveNagativeZero {
+
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-10);
+    }
+    public  static  void checkNumber(int number) {
+        if(number > 0) {
+            System.out.println("Positive");
+        } else if(number < 0) {
+            System.out.println("Negative");
+        }
+    }
+}

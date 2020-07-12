1b67fcad9ad5ce0ed5f6f2e15c72d856b8ac2738
Aadil-Abdirizak <65042842+Aadil-Abdirizak@users.noreply.github.com>
2020-07-12 14:37:08 +0300

    Add files via upload

---
+++ maina.java
@@ -0,0 +1,12 @@
+public class maina {
+
+        public static void  main(String[] args) {
+
+            long miles = speedConverter.toMilesPerHour(10.5);
+            System.out.println("Miles = " + miles);
+
+            peedConverter.printConversion(10.5);
+        }
+    }
+
+
